0e0f68ef6d2f5b52036e8758b2b2389e2ee6240b
Leonardo Medina <leonarmed@gmail.com>
2022-08-06 02:58:16 +0000

    initial upload

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+  var ext = [".com", ".net", ".us", ".io", ".ve", ".py"];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let z = 0; z < ext.length; z++) {
+          console.log(pronoun[i] + adj[j] + noun[k] + ext[z]);
+        }
+      }
+    }
+  }
 };
